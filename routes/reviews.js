1703aeae40c9dccbc5c3a472ac4453ac50540c81
Tarjei Ylvisåker <trylvis@outlook.com>
2018-12-30 17:35:13 +0100

    Add review routes

---
+++ routes/reviews.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+
+// GET reviews index /posts/:id/reviews 
+router.get('/', (req, res, next) => {
+    res.send('INDEX /reviews');
+});
+
+// POST reviews create /posts/:id/reviews/:review_id
+router.post('/', (req, res, next) => {
+  res.send('CREATE /reviews');
+});
+
+// GET reviews edit /posts/:id/reviews/:review_id/edit
+router.get('/:review_id/edit', (req, res, next) => {
+  res.send('EDIT /reviews/:id/edit');
+});
+
+// PUT reviews update /posts/:id/reviews/:review_id
+router.put('/:review_id', (req, res, next) => {
+  res.send('UPDATE /reviews/:id');
+});
+  
+// DELETE reviews destroy /posts/:id/reviews/:review_id
+router.delete('/:review_id', (req, res, next) => {
+  res.send('DELETE /reviews/:id');
+});
+
+
+
+  module.exports = router;
