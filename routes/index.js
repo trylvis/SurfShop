03521eba227ddf5471ab6c6cbdc120941de6f5fa
Tarjei Ylvisåker <trylvis@outlook.com>
2018-12-30 18:11:44 +0100

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,65 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+//
+// REGISTER
+//
+
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register')
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register')
+});
+
+//
+// LOGIN
+//
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login')
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login')
+});
+
+//
+// Profile
+//
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile')
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile', (req, res, next) => {
+  res.send('PUT /profile')
+});
+
+/* GET /forgot */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot')
+});
+
+/* PUT /forgot */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot')
+});
+
+/* GET /reset */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token')
+});
+
+/* PUT /reset */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset/:token')
+});
 module.exports = router;
